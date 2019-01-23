40def9790813cd8d23cfae8b701d1b280d16cdb5
Harputa <harputa@users.noreply.github.com>
2019-01-23 18:33:08 +0100

    Test oniki added

--- spring/demo-spring-boot/src/main/java/com/mastertheboss/HomeController.java
+++ spring/demo-spring-boot/src/main/java/com/mastertheboss/HomeController.java
@@ -27,6 +27,6 @@ public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String ipaddress() throws Exception {
-        return "Reply: " + welcome + " Allah Birdir";
+        return "Reply: Test oniki " + welcome;
     }
 }
